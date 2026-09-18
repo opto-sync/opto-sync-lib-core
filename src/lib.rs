8ed435ae00e860aa77e7e486b4616bc329a68234
Alexander Mills <alex@oresoftware.com>
2026-09-18 15:03:58 -0500

    style(config): apply rustfmt to exports

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,10 @@ pub mod schema;
 
 pub use config::CoreConfig;
 pub use config_discovery::{
-    discover_opto_sync_config, discover_opto_sync_config_from_cwd,
-    load_nearest_opto_sync_config, load_nearest_opto_sync_config_from_cwd,
-    load_nearest_opto_sync_config_from_cwd_with_source,
-    load_nearest_opto_sync_config_with_source, DiscoveredOptoSyncConfig,
-    LoadedOptoSyncConfig, OptoSyncConfigDiscoveryError, MAX_DISCOVERY_ANCESTORS,
+    discover_opto_sync_config, discover_opto_sync_config_from_cwd, load_nearest_opto_sync_config,
+    load_nearest_opto_sync_config_from_cwd, load_nearest_opto_sync_config_from_cwd_with_source,
+    load_nearest_opto_sync_config_with_source, DiscoveredOptoSyncConfig, LoadedOptoSyncConfig,
+    OptoSyncConfigDiscoveryError, MAX_DISCOVERY_ANCESTORS,
 };
 pub use connection::CorePool;
 pub use error::CoreError;
